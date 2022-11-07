05b03458933421c5026efd9b9b81bcc1d0042278
羽墨 <69838566+yumo0@users.noreply.github.com>
2022-11-07 14:16:39 +0800

    cicd test

--- src/main/java/top/yumoyumo/yumobot/controller/HelpController.java
+++ src/main/java/top/yumoyumo/yumobot/controller/HelpController.java
@@ -36,4 +36,21 @@ public class HelpController {
                         """
         );
     }
+
+
+    @RequestMapping("/help2")
+    public String help2() {
+        return
+                """
+                        YumoBot
+                        --------------------
+                        0. /版本\t查看版本更新日志
+                        1. /人设\t查看bot人设
+                        2. /城市\t查询城市信息
+                        3. /天气\t查询天气信息
+                        4. /一言\t一言
+                        5. 闲聊模式：关闭:<雪豹闭嘴>;开启<雪豹张嘴>
+                        6. /图片\t获取图片.
+                        """;
+    }
 }
