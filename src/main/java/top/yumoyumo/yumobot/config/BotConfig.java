dfc22ef98c8919a664c76a7285f01dc6237fe9d3
羽墨 <69838566+yumo0@users.noreply.github.com>
2023-02-13 19:21:46 +0800

    bug fix

--- src/main/java/top/yumoyumo/yumobot/config/BotConfig.java
+++ src/main/java/top/yumoyumo/yumobot/config/BotConfig.java
@@ -9,7 +9,6 @@ import net.mamoe.mirai.utils.BotConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import top.yumoyumo.yumobot.listener.ChatListener;
 import top.yumoyumo.yumobot.listener.CloseListener;
 import top.yumoyumo.yumobot.listener.DispatcherListener;
@@ -52,7 +51,7 @@ public class BotConfig {
             {
                 //保存设备信息到文件deviceInfo.json文件里相当于是个设备认证信息
                 fileBasedDeviceInfo(deviceInfo);
-                setProtocol(MiraiProtocol.ANDROID_PAD); // 切换协议
+                setProtocol(MiraiProtocol.ANDROID_PHONE); // 切换协议
                 setHeartbeatStrategy(HeartbeatStrategy.STAT_HB); // 切换心跳策略
                 redirectBotLogToDirectory();
             }
